80733e64ee3ff18c594034248d04965336a78aaa
Petr Karmashev <petr.karmashev@iqoption.com>
2017-08-29 12:04:16 +0300

    Print message on SIGINT (Ctrl+C) before exit

--- interrupt.go
+++ interrupt.go
@@ -1,22 +1,30 @@
 package osinterrupt
 
 import (
-    "os"
-    "os/signal"
+	"fmt"
+	"os"
+	"os/signal"
 )
 
 // Handle SIGINT (Ctrl+C) and call callback function before exit
 func HandleInterruptSignal(callback func()) {
-    signals := make(chan os.Signal, 1)
-    signal.Notify(signals, os.Interrupt)
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
 
-    go func(){
-        for osSignal := range signals {
-            // if signal is ^C (SIGINT)
-            if osSignal.String() == "interrupt" {
-                callback()
-                os.Exit(0)
-            }
-        }
-    }()
+	go func() {
+		for osSignal := range signals {
+			// if signal is ^C (SIGINT)
+			if osSignal.String() == "interrupt" {
+				callback()
+				os.Exit(0)
+			}
+		}
+	}()
+}
+
+// Print message on SIGINT (Ctrl+C) before exit
+func PrintMessageOnInterruptSignal(message string) {
+	HandleInterruptSignal(func() {
+		fmt.Println(message)
+	})
 }
